5fc0bc56ff520298329f3b10dd125c6107512187
tatyanayavkina <t.yavkina@mail.ru>
2015-10-08 12:32:05 +0300

    startServer

--- src/StartServer.java
+++ src/StartServer.java
@@ -12,14 +12,17 @@ import java.util.HashMap;
 public class StartServer {
 
     public static void main(String[] args) {
-        Config config = XMLReader.readParams();
-        HashMap<String, String> users = XMLReader.readUsers();
-
         try{
+            Config config = XMLReader.readParams();
+            HashMap<String, String> users = XMLReader.readUsers();
             ServerProcessor ServerProcessor = new ServerProcessor(config, users);
             ServerProcessor.start();
-        } catch (IOException e){
-
+        } catch(IOException e){
+            System.out.println("ServerProcessor creation error");
+            System.exit(-1);
+        } catch (Exception e){
+            System.out.println("Params reading error");
+            System.exit(-1);
         }
 
     }
--- src/utils/XMLReader.java
+++ src/utils/XMLReader.java
@@ -23,20 +23,15 @@ public class XMLReader {
     private static final String name = "name";
     private static final String password = "password";
 
-    private static Element getRootElement(String path){
-        Element root = null;
+    private static Element getRootElement(String path) throws Exception{
+        Element root;
 
-        try{
-            File fXmlFile = new File(path);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(fXmlFile);
-            doc.getDocumentElement().normalize();
-            root = doc.getDocumentElement();
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        File fXmlFile = new File(path);
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(fXmlFile);
+        doc.getDocumentElement().normalize();
+        root = doc.getDocumentElement();
 
         return root;
     }
@@ -51,45 +46,35 @@ public class XMLReader {
         return value;
     }
 
-    public static Config readParams(){
-        Config config = null;
-        try{
-            Element root = getRootElement(pathToParams);
+    public static Config readParams() throws Exception{
+        Config config;
+        Element root = getRootElement(pathToParams);
 
-            String hostValue = getTextValue(root, host);
-            String portValue = getTextValue(root, port);
-            String messagesLimitValue = getTextValue(root, message);
-            String threadCountValue = getTextValue(root, thread);
+        String hostValue = getTextValue(root, host);
+        String portValue = getTextValue(root, port);
+        String messagesLimitValue = getTextValue(root, message);
+        String threadCountValue = getTextValue(root, thread);
 
-            config = new Config(hostValue, portValue, messagesLimitValue, threadCountValue);
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        config = new Config(hostValue, portValue, messagesLimitValue, threadCountValue);
 
         return config;
     }
 
-    public static HashMap<String,String> readUsers(){
+    public static HashMap<String,String> readUsers() throws Exception{
         HashMap<String,String> usersMap = new HashMap<String,String>();
 
-        try{
-            Element root = getRootElement(pathToUsers);
+        Element root = getRootElement(pathToUsers);
 
-            NodeList nList = root.getElementsByTagName(user);
-            int nListLength = nList.getLength();
+        NodeList nList = root.getElementsByTagName(user);
+        int nListLength = nList.getLength();
 
-            for( int i = 0; i <nListLength; i++ ){
-                Element nElement = (Element) nList.item(i);
+        for( int i = 0; i <nListLength; i++ ){
+            Element nElement = (Element) nList.item(i);
 
-                String nameValue = getTextValue(nElement, name);
-                String passwordValue = getTextValue(nElement, password);
+            String nameValue = getTextValue(nElement, name);
+            String passwordValue = getTextValue(nElement, password);
 
-                usersMap.put(nameValue, passwordValue);
-            }
-
-        } catch (Exception e){
-            e.printStackTrace();
+            usersMap.put(nameValue, passwordValue);
         }
 
         return usersMap;
