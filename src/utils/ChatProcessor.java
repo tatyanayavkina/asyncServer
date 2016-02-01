ee81e861b0bf276997cb04e6d068203563111bdd
Tatyana <t.yavkina@mail.ru>
2016-02-01 09:33:45 +0300

    Update ChatProcessor.java

--- src/utils/ChatProcessor.java
+++ src/utils/ChatProcessor.java
@@ -2,9 +2,6 @@ package utils;
 
 import handlers.ChannelAndBuffersContainer;
 
-/**
- * Created on 25.09.2015.
- */
 public interface ChatProcessor {
     void handleAuthorization(String credentials, ChannelAndBuffersContainer channelAndBuffersContainer);
     void handleInputMessage(String messageString, ChannelAndBuffersContainer channelAndBuffersContainer);
