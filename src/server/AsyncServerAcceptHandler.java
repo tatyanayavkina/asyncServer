7c540eb203a342953725d199b2dfffdd619f5bf5
Tatyana <t.yavkina@mail.ru>
2016-02-01 09:33:08 +0300

    Update AsyncServerAcceptHandler.java

--- src/server/AsyncServerAcceptHandler.java
+++ src/server/AsyncServerAcceptHandler.java
@@ -6,9 +6,7 @@ import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
 
-/**
- * Created on 15.09.2015.
- */
+
 public class AsyncServerAcceptHandler implements CompletionHandler<AsynchronousSocketChannel, ChannelAndBuffersContainer> {
     private final ServerProcessor serverProcessor;
     private final AsynchronousServerSocketChannel serverChannel;
